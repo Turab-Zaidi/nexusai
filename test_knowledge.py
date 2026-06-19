fbc7fa2c01ddb445740fac339c3901d5c254a8dc
Turab Zaidi <turab.z567@gmail.com>
2026-06-19 12:15:33 +0530

    Cleanup: remove test scripts

--- test_knowledge.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from core.agents.knowledge_agent import KnowledgeAgent
-
-async def main():
-    agent = KnowledgeAgent()
-    res = await agent.run('tell me about return policy of bans', 'general_inquiry', '')
-    print('RESPONSE:', repr(res.output.get('response')))
-
-asyncio.run(main())
